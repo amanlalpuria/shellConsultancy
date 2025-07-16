706707d338faea6447fb9314a22d7977ddb35b77
Aman Lalpuria <aman.sharma163@gmail.com>
2025-07-16 10:15:36 +0530

    Deploy static site to gh-pages

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // basePath: '/shellConsultancy',
+  // assetPrefix: './',
+};
+
+export default nextConfig;
